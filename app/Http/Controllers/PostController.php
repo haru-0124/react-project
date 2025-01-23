c6e843ddcc50983d031566ff881bc53742ac0a9c
haru-0124 <k.haruto0124@au.com>
2025-01-23 05:43:15 +0000

    post_show_detail

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,10 @@ class PostController extends Controller
     {
         return Inertia::render("Post/Index",["posts" => $post->get()]);
     }
+    
+    public function show(Post $post)
+    {
+        return Inertia::render("Post/Show", ["post" => $post]);
+    }
+
 }
